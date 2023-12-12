15cd2325a68f7a111f30d16157d66e23b2022176
Geertsky <122536131+Geertsky@users.noreply.github.com>
2023-12-12 13:48:52 +0100

    corrected to include all excludes

--- 94bambini/print-python-includes.py
+++ 94bambini/print-python-includes.py
@@ -2,8 +2,11 @@
 #excludes is a list or regexps to exclude
 excludes=['^/home']
 import os,sys,re
+
 for P in sys.path:
+    RES=True
     for R in excludes:
-        if not re.search(R,P):
-            if os.path.exists(P):
-                print(P)
+        if re.search(R,P):
+            RES=False
+    if RES and os.path.exists(P):
+        print(P)
