7797c4b20ad6149530709e98076b027f96eb35fd
geertsky <geert@verweggistan.eu>
2024-06-07 16:55:53 +0000

    delete unneeded packages

--- conda-recipes/automake/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#./bootstrap.sh
-echo |cpan Thread::Queue
-./configure --prefix=$PREFIX
-make
-make install
--- conda-recipes/libffi/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-./autogen.sh
-./configure --prefix=$PREFIX       \
-            --disable-static       \
-            --with-gcc-arch=native \
-            --disable-exec-static-tramp
-make&&make check&&make install
