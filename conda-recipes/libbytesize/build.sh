a1e3f39e3d001383b9a51bdea770fd9b60da9d58
geertsky <geert@verweggistan.eu>
2024-06-26 20:09:54 +0200

    build without gtk-doc

--- conda-recipes/libbytesize/build.sh
+++ conda-recipes/libbytesize/build.sh
@@ -1,11 +1,9 @@
-if [ -z ${CONDA_BUILD+x} ]; then
-    source /home/geert/miniforge3/conda-bld/libbytesize_1711284366822/work/build_env_setup.sh
-fi
 #!/bin/bash
 
 export CFLAGS=$CFLAGS" -I$BUILD_PREFIX/include" # -L$BUILD_PREFIX/lib"
 ./configure \
   --prefix=$PREFIX \
-  --with-python-sys-prefix
+  --with-python-sys-prefix \
+  --with-gtk-doc=no
 make
 make install
