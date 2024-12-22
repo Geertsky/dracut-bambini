1fa62ad46cb832a5cece41ceb0e1f60ff429e92b
geertsky <geert@verweggistan.eu>
2024-12-22 21:00:46 +0000

    94bambini/module-setup.sh: Remove dynamic list of binaries
    94bambini/module-setup.sh: modify required binaries + test for bambini-python.squashfs
    
    module-setup.sh: add insmod + comment create-lvm-links.sh inst_hook

--- 94bambini/module-setup.sh
+++ 94bambini/module-setup.sh
@@ -5,9 +5,12 @@
 
 # called by dracut
 check() {
-  cd "${moddir}"
-  #using a text files to keep things dynamic for now...
-  require_binaries $(cat binaries) || return 1
+  require_binaries dirname ssh-keygen /usr/libexec/openssh/sshd-keygen|| return 1
+
+  [[ -r "${moddir}/bambini-python.squashfs" ]] || {
+        derror "Missing ${moddir}/bambini-python.squashfs. See dracut-bambini/conda-recipes/bambini-python.yml"
+        return 1
+  }
   # 0 enables by default, 255 only on request
   return 255
 }
@@ -19,11 +22,9 @@ depends() {
 
 # called by dracut
 install() {
-  #Install binaries and additional includes
-  cd "${moddir}"
-  #using a text files to keep things dynamic for now...
-  inst_multiple $(cat binaries)
-  inst_multiple /usr/lib/rpm/rpmrc /usr/lib/rpm/macros /usr/lib/rpm/redhat/rpmrc
+  # dirname is needed for conda/bin/activate... Not required but useful for debugging
+  inst /usr/bin/dirname
+  inst chmod
 
   #check if internal-sftp is enabled otherwise enable it here
   if ! grep -q internal-sftp "${initdir}"/etc/ssh/sshd_config; then
@@ -31,14 +32,6 @@ install() {
     awk '!found && /^AcceptEnv/ { print "Subsystem sftp                  internal-sftp"; found=1 } 1' "${initdir}/etc/ssh/sshd_config.bak" >"${initdir}/etc/ssh/sshd_config"
   fi
 
-  #install packed conda environment and python binary for glibc dep. resolution.
-  inst "${moddir}/bambini-python.tar.gz" "/tmp/bambini-python.tar.gz"
-  dd if="/dev/urandom" of="${initdir}/placeholder.img" bs=1M count=500 >/dev/null 2>&1
-  PTMP="$(mktemp -d)"
-  tar -xf "${moddir}/bambini-python.tar.gz" -C "$PTMP" "bin/python3*"
-  PYTHON=$(find ${PTMP} -type f -exec file {} \;|tr -d ":"|awk '{if ($2=="ELF") print $1}')
-  inst "${PYTHON}" "/bin/python"
-  rm -Rf "${PTMP}"
-
-  inst_hook cmdline 40 "${moddir}/create-lvm-links.sh"
+  # Install lvm links creation script
+#  inst_hook cmdline 40 "${moddir}/create-lvm-links.sh"
 }
