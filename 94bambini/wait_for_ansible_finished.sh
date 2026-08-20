2bc72bdb05d911f453ef5df24d04fb3160c4faae
Geert Geurts <geert@verweggistan.eu>
2026-08-20 10:55:01 +0200

    wait_for_ansible_finished.sh: extract message just before echo

--- 94bambini/wait_for_ansible_finished.sh
+++ 94bambini/wait_for_ansible_finished.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/sh
+export PATH=$PATH:/local/conda/envs/bambini-python/bin
+
 type info >/dev/null 2>&1 || . /lib/dracut-lib.sh
 info "Waiting for Ansible to create me a roofs..."
+X=0
+MSG=""
 while [ ! -f "/tmp/ansible-finished" ]; do
+  let X+=5
   sleep 5
+  [ -f /tmp/message ]&&MSG=$(cat /tmp/message)
+  echo "$(date -u -d @${X} +'%T'): Waiting for Ansible; $MSG" >>/dev/console
 done
