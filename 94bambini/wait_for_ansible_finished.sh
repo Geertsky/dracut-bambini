4699e04166c9dde2d3ec73fce83e869672aa5803
Geert Geurts <geert@verweggistan.eu>
2026-08-21 09:36:55 +0200

    wait_for_ansible_finished.sh: Take uptime as initial value for X

--- 94bambini/wait_for_ansible_finished.sh
+++ 94bambini/wait_for_ansible_finished.sh
@@ -3,7 +3,7 @@ export PATH=$PATH:/local/conda/envs/bambini-python/bin
 
 type info >/dev/null 2>&1 || . /lib/dracut-lib.sh
 info "Waiting for Ansible to create me a roofs..."
-X=0
+X=$(awk '{print int($1)}' /proc/uptime)
 MSG=""
 while [ ! -f "/tmp/ansible-finished" ]; do
   let X+=5
